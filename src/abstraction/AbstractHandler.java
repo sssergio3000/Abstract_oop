a937943e6895f97c48fd28b8aad1a3e79ce7f789
Sergey Khaykov <ssserg3000@gmail.com>
2022-11-07 21:00:11 +0200

    Initial commit

---
+++ src/abstraction/AbstractHandler.java
@@ -0,0 +1,8 @@
+package abstraction;
+
+abstract class AbstractHandler {
+    abstract void open();
+    abstract void create();
+    abstract void change();
+    abstract void save();
+}
---
+++ src/abstraction/TXTHandler.java
@@ -0,0 +1,23 @@
+package abstraction;
+
+public class TXTHandler  extends AbstractHandler{
+    @Override
+    void open() {
+        System.out.println("Open TXT");
+    }
+
+    @Override
+    void create() {
+        System.out.println("Create TXT");
+    }
+
+    @Override
+    void change() {
+        System.out.println("Change TXT");
+    }
+
+    @Override
+    void save() {
+        System.out.println("Save TXT");
+    }
+}
